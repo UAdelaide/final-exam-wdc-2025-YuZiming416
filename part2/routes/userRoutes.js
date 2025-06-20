8a32fbb5e7c93aec0eb5854e6a1ad4e5972cd654
YuZiming416 <Yu416CN@outlook.com>
2025-06-20 13:47:56 +0000

    [automatic commit] 2025-06-20 13:47:56 UTC 175.35.80.208

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -49,7 +49,6 @@ router.post('/login', async (req, res) => {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
 
-
     const user = rows[0];
     req.session.user = { id: user.user_id, role: user.role };
     res.json({ message: 'Login successful', user });
