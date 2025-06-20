d86c6296f1b071868afed11f8bd72c64a6ec7b5f
YuZiming416 <Yu416CN@outlook.com>
2025-06-20 06:05:42 +0000

    [automatic commit] 2025-06-20 06:05:42 UTC 180.150.112.54

--- part1/routes/walkrequests.js
+++ part1/routes/walkrequests.js
@@ -2,4 +2,6 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db');
 
-router.get('/open', async)
+router.get('/open', async (req, res) => {
+    try {}
+})
