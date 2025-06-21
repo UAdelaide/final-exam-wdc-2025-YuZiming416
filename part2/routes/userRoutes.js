4e8d8eb680580e32f63c320a9ab8fbfacef1d406
YuZiming416 <Yu416CN@outlook.com>
2025-06-21 04:13:35 +0000

    [automatic commit] 2025-06-21 04:13:35 UTC 175.35.80.208

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -65,7 +65,7 @@ router.post('/logout', (req, res) => {
   req.session.destroy(err => {
     if (err) {
       console.error('Session destroy error:', err);
-      return res.status(500).json({})
+      return res.status(500).json({ error: 'Logout failed ' });
     }
   })
 })
