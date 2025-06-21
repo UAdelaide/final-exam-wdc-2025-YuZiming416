d91967a4f7373d07bf5cb6a99aaf9a3ed9c12a9b
YuZiming416 <Yu416CN@outlook.com>
2025-06-21 05:13:15 +0000

    [automatic commit] 2025-06-21 05:13:15 UTC 175.35.80.208

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -62,7 +62,6 @@ router.post('/login', async (req, res) => {
 
 router.post('/logout', (req, res) => {
   // destroy the session
-  // eslint-disable-next-line consistent-return
   req.session.destroy((err) => {
     if (err) {
       console.error('Session destroy error:', err);
